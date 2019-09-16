2d4fe52f40afce6b67ec07a3596e3ecec80b80e9
Erica Sim <ejsim@uw.edu>
2019-09-15 22:34:33 -0700

    addin' my bot

---
+++ dialogs/mainDialog.js
@@ -0,0 +1,161 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { TimexProperty } = require('@microsoft/recognizers-text-data-types-timex-expression');
+const { MessageFactory, InputHints } = require('botbuilder');
+const { LuisRecognizer } = require('botbuilder-ai');
+const { ComponentDialog, DialogSet, DialogTurnStatus, TextPrompt, WaterfallDialog } = require('botbuilder-dialogs');
+
+const MAIN_WATERFALL_DIALOG = 'mainWaterfallDialog';
+
+class MainDialog extends ComponentDialog {
+    constructor(luisRecognizer, recommendDialog) {
+        super('MainDialog');
+
+        if (!luisRecognizer) throw new Error('[MainDialog]: Missing parameter \'luisRecognizer\' is required');
+        this.luisRecognizer = luisRecognizer;
+
+        if (!recommendDialog) throw new Error('[MainDialog]: Missing parameter \'recommendDialog\' is required');
+
+        // Define the main dialog and its related components.
+        // This is a sample "book a flight" dialog.
+        this.addDialog(new TextPrompt('TextPrompt'))
+            .addDialog(recommendDialog)
+            .addDialog(new WaterfallDialog(MAIN_WATERFALL_DIALOG, [
+                this.introStep.bind(this),
+                this.actStep.bind(this),
+                this.finalStep.bind(this)
+            ]));
+
+        this.initialDialogId = MAIN_WATERFALL_DIALOG;
+    }
+
+    /**
+     * The run method handles the incoming activity (in the form of a TurnContext) and passes it through the dialog system.
+     * If no dialog is active, it will start the default dialog.
+     * @param {*} turnContext
+     * @param {*} accessor
+     */
+    async run(turnContext, accessor) {
+        const dialogSet = new DialogSet(accessor);
+        dialogSet.add(this);
+
+        const dialogContext = await dialogSet.createContext(turnContext);
+        const results = await dialogContext.continueDialog();
+        if (results.status === DialogTurnStatus.empty) {
+            await dialogContext.beginDialog(this.id);
+        }
+    }
+
+    /**
+     * First step in the waterfall dialog. Prompts the user for a command.
+     * Currently, this expects a booking request, like "book me a flight from Paris to Berlin on march 22"
+     * Note that the sample LUIS model will only recognize Paris, Berlin, New York and London as airport cities.
+     */
+    async introStep(stepContext) {
+        if (!this.luisRecognizer.isConfigured) {
+            const messageText = 'NOTE: LUIS is not configured. To enable all capabilities, add `LuisAppId`, `LuisAPIKey` and `LuisAPIHostName` to the .env file.';
+            await stepContext.context.sendActivity(messageText, null, InputHints.IgnoringInput);
+            return await stepContext.next();
+        }
+
+        const messageText = stepContext.options.restartMsg ? stepContext.options.restartMsg : 'Hello! What kind of restaurant are you looking for?';
+        const promptMessage = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+        return await stepContext.prompt('TextPrompt', { prompt: promptMessage });
+    }
+
+    /**
+     * Second step in the waterfall.  This will use LUIS to attempt to extract the origin, destination and travel dates.
+     * Then, it hands off to the bookingDialog child dialog to collect any remaining details.
+     */
+    async actStep(stepContext) {
+        const recDetails = {};
+
+        if (!this.luisRecognizer.isConfigured) {
+            // LUIS is not configured, we just run the BookingDialog path.
+            return await stepContext.beginDialog('recommendDialog', recDetails);
+        }
+
+        // Call LUIS and gather any potential booking details. (Note the TurnContext has the response to the prompt)
+        const luisResult = await this.luisRecognizer.executeLuisQuery(stepContext.context);
+        switch (LuisRecognizer.topIntent(luisResult)) {
+        case 'FindRestaurant':
+            // Extract the values for the composite entities from the LUIS result.
+            //const fromEntities = this.luisRecognizer.getFromEntities(luisResult);
+            //const toEntities = this.luisRecognizer.getToEntities(luisResult);
+            const cuisineEntity = this.luisRecognizer.getCuisineEntity(luisResult);
+            const priceEntity = this.luisRecognizer.getPriceEntity(luisResult);
+            const deliveryEntity = this.luisRecognizer.getDeliveryEntity(luisResult);
+
+            // Show a warning for Origin and Destination if we can't resolve them.
+            // await this.showWarningForUnsupportedCities(stepContext.context, fromEntities, toEntities);
+            // TODO do this for unsupported cuisines?  
+
+            // Initialize BookingDetails with any entities we may have found in the response.
+            recDetails.cuisine = cuisineEntity;
+            recDetails.price = priceEntity;
+            recDetails.delivery = deliveryEntity;
+            console.log('LUIS extracted these details:', JSON.stringify(recDetails));
+
+            // Run the BookingDialog passing in whatever details we have from the LUIS call, it will fill out the remainder.
+            return await stepContext.beginDialog('recommendDialog', recDetails);
+
+
+        default:
+            // Catch all for unhandled intents
+            const didntUnderstandMessageText = `Sorry, I didn't get that. Please try asking in a different way (intent was ${ LuisRecognizer.topIntent(luisResult) })`;
+            await stepContext.context.sendActivity(didntUnderstandMessageText, didntUnderstandMessageText, InputHints.IgnoringInput);
+        }
+
+        return await stepContext.next();
+    }
+
+    /**
+     * Shows a warning if the requested From or To cities are recognized as entities but they are not in the Airport entity list.
+     * In some cases LUIS will recognize the From and To composite entities as a valid cities but the From and To Airport values
+     * will be empty if those entity values can't be mapped to a canonical item in the Airport.
+     */
+    async showWarningForUnsupportedCities(context, fromEntities, toEntities) {
+        const unsupportedCities = [];
+        if (fromEntities.from && !fromEntities.airport) {
+            unsupportedCities.push(fromEntities.from);
+        }
+
+        if (toEntities.to && !toEntities.airport) {
+            unsupportedCities.push(toEntities.to);
+        }
+
+        if (unsupportedCities.length) {
+            const messageText = `Sorry but the following airports are not supported: ${ unsupportedCities.join(', ') }`;
+            await context.sendActivity(messageText, messageText, InputHints.IgnoringInput);
+        }
+    }
+
+    /**
+     * This is the final step in the main waterfall dialog.
+     * It wraps up the sample "book a flight" interaction with a simple confirmation.
+     */
+    async finalStep(stepContext) {
+        // If the child dialog ("bookingDialog") was cancelled or the user failed to confirm, the Result here will be null.
+        if (stepContext.result) {
+            const result = stepContext.result;
+            // Now we have all the booking details.
+
+            // This is where calls to the booking AOU service or database would go. <----- NOTE!!!
+            // UNLESS I ask for confirmation 
+
+            // If the call to the booking service was successful tell the user.
+            //const timeProperty = new TimexProperty(result.travelDate);
+            //const travelDateMsg = timeProperty.toNaturalLanguage(new Date(Date.now()));
+            const msg = `I have that you're looking for a ${ result.price } ${ result.cuisine } for ${ result.delivery }.`;
+            // TODO! ask for confirmation???? --- probably would want a confirmation step before final one
+
+            await stepContext.context.sendActivity(msg, msg, InputHints.IgnoringInput);
+        }
+
+        // Restart the main dialog with a different message the second time around
+        return await stepContext.replaceDialog(this.initialDialogId, { restartMsg: 'What else can I do for you?' });
+    }
+}
+
+module.exports.MainDialog = MainDialog;
---
+++ dialogs/recommendDialog.js
@@ -0,0 +1,116 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { TimexProperty } = require('@microsoft/recognizers-text-data-types-timex-expression');
+const { InputHints, MessageFactory } = require('botbuilder');
+const { ConfirmPrompt, TextPrompt, ChoiceFactory, ChoicePrompt, WaterfallDialog } = require('botbuilder-dialogs');
+const { CancelAndHelpDialog } = require('./cancelAndHelpDialog');
+const { DateResolverDialog } = require('./dateResolverDialog');
+
+
+const CONFIRM_PROMPT = 'confirmPrompt';
+const DATE_RESOLVER_DIALOG = 'dateResolverDialog';
+const TEXT_PROMPT = 'textPrompt';
+const CHOICE_PROMPT = "CHOICE_PROMPT";
+const WATERFALL_DIALOG = 'waterfallDialog';
+
+class RecommendDialog extends CancelAndHelpDialog {
+    constructor(id) {
+        super(id || 'recommendDialog');
+
+        this.addDialog(new TextPrompt(TEXT_PROMPT))
+            .addDialog(new ChoicePrompt(CHOICE_PROMPT))
+            .addDialog(new ConfirmPrompt(CONFIRM_PROMPT))
+            .addDialog(new DateResolverDialog(DATE_RESOLVER_DIALOG))
+            .addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
+                this.cuisineStep.bind(this),
+                this.priceStep.bind(this),
+                this.deliveryStep.bind(this),
+                this.confirmStep.bind(this),
+                this.finalStep.bind(this)
+            ]));
+
+        this.initialDialogId = WATERFALL_DIALOG;
+    }
+
+    /**
+     * If a cuisine has not been provided, prompt for one.
+     */
+    async cuisineStep(stepContext) {
+        const recDetails = stepContext.options;
+
+        if (!recDetails.cuisine) {
+            const messageText = 'What kind of food do you want?';
+            const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+            return await stepContext.prompt(TEXT_PROMPT, { prompt: msg });
+        }
+        return await stepContext.next(recDetails.cuisine);
+    }
+
+    /**
+     * If a price point has not been provided, prompt for one.
+     */
+    async priceStep(stepContext) {
+        const recDetails = stepContext.options;
+
+        // Capture the response to the previous step's prompt
+        recDetails.cuisine = stepContext.result;
+        if (!recDetails.price) {
+            const messageText = 'What price point are you looking for?';
+            const msg = MessageFactory.text(messageText, 'What price point are you looking for?', InputHints.ExpectingInput);
+            return await stepContext.prompt(CHOICE_PROMPT, { prompt: msg,
+            choices: ChoiceFactory.toChoices(["cheap", "moderate", "average"]) });
+        }
+        return await stepContext.next(recDetails.price);
+    }
+
+    /**
+     * If a delivery option has not been provided, prompt for one.
+     */
+    async deliveryStep(stepContext) {
+        const recDetails = stepContext.options;
+
+        // Capture the results of the previous step
+        recDetails.price = stepContext.result;
+        if (!recDetails.delivery) {
+            const messageText = 'Would you like delivery?';
+            const msg = MessageFactory.text(messageText, 'Would you like delivery?', InputHints.ExpectingInput);
+            return await stepContext.prompt(CHOICE_PROMPT, { prompt: msg,
+            choices: ChoiceFactory.toChoices(["delivery", "take-out", "dine-in"]) });
+        }
+        return await stepContext.next(recDetails.delivery);
+    }
+
+    /**
+     * Confirm the information the user has provided.
+     */
+    async confirmStep(stepContext) {
+        const recDetails = stepContext.options;
+
+        // Capture the results of the previous step
+        recDetails.delivery = stepContext.result;
+        const messageText = `Please confirm, I have you looking for a ${ recDetails.price.value } ${ recDetails.cuisine.value } restaurant for ${ recDetails.delivery.value }. Is this correct?`;
+        const msg = MessageFactory.text(messageText, messageText, InputHints.ExpectingInput);
+
+        // Offer a YES/NO prompt.
+        return await stepContext.prompt(CONFIRM_PROMPT, { prompt: msg });
+    }
+
+    /**
+     * Complete the interaction and end the dialog.
+     */
+    async finalStep(stepContext) {
+        if (stepContext.result === true) {
+            const recDetails = stepContext.options;
+            return await stepContext.endDialog(recDetails);
+        }
+        return await stepContext.endDialog();
+    }
+
+    isAmbiguous(timex) {
+        const timexPropery = new TimexProperty(timex);
+        return !timexPropery.types.has('definite');
+    }
+}
+
+module.exports.RecommendDialog = RecommendDialog;
